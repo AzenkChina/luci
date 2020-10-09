c4986b09d6e5b6365d95783d51729f40266ab96b
AzenkChina <1179228661@qq.com>
2020-10-09 23:33:55 +0800

    :Modify cbi of serial.

--- applications/luci-app-serial/luasrc/model/cbi/serial.lua
+++ applications/luci-app-serial/luasrc/model/cbi/serial.lua
@@ -4,11 +4,21 @@
 
 m = Map("ser2net", translate("Serial"), translate("Serial port proxy"))
 
-s = m:section(TypedSection, "ser2net", "")
-s.addremove = false
-s.anonymous = true
+g = m:section(NamedSection, "global", "ser2net", "")
+g.addremove = false
+g.anonymous = false
 
-s:tab("Global", translate("Global Configuration"))
-enabled = s:taboption("basic", Flag, "enabled", translate("Enable"))
+g1 = g:option(Flag, "enabled", "enable", translate("enable"))
 
-return m
+d = m:section(TypedSection, "default", "")
+d.addremove = false
+d.anonymous = true
+
+d1 = d:option(ListValue, "speed", "Baudrate", translate("Baudrate"))
+d1:value("300",   "300")
+d1:value("600",   "600")
+d1:value("1200",  "1200")
+d1:value("2400",  "2400")
+d1:value("4800",  "4800")
+d1:value("9600",  "9600")
+d1:value("19200", "19200")
