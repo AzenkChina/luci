940bebf6078667d1650bc4427b3d22d3d8a4c93e
azenk <azenk@Debian.domain>
2020-10-19 17:27:58 +0800

    Optimize luci-app-collector.

--- applications/luci-app-collector/luasrc/model/cbi/collector.lua
+++ applications/luci-app-collector/luasrc/model/cbi/collector.lua
@@ -14,15 +14,15 @@ g1 = g:option(Flag, "enable", translate("Enabled"))
 g1.rmempty = false
 
 g2 = g:option(ListValue, "period", translate("Collect period"))
-g2:value("5",    "5 minutes")
-g2:value("10",  "10 minutes")
-g2:value("15",  "15 minutes")
-g2:value("30",  "30 minutes")
-g2:value("60",  "60 minutes")
+g2:value("5",  translate("5 minutes")
+g2:value("10", translate("10 minutes")
+g2:value("15", translate("15 minutes")
+g2:value("30", translate("30 minutes")
+g2:value("60", translate("60 minutes")
 
 g3 = g:option(Value, "client", translate("Client address"))
 g3.rmempty = false
-g3.datatype = "and(uinteger,min(1),max(16383))"
+g3.datatype = "and(uinteger,min(1),max(127))"
 
 g4 = g:option(Value, "logical", translate("Server logical address"))
 g4.rmempty = false
@@ -41,6 +41,24 @@ g7 = g:option(ListValue, "level", translate("Access level"))
 g7:value("none", translate("None"))
 g7:value("low",  translate("Password"))
 g7:value("high", translate("Encryption"))
+
+g8 = g:option(Value, "password", translate("Password"))
+g8.rmempty = false
+g8.maxlength = 40
+g8.size = 40
+g8:depends({level="low"})
+
+g9 = g:option(Value, "ekey", translate("Encryption key"))
+g9.rmempty = false
+g9.size = 40
+g9.maxlength = 32
+g9:depends({level="high"})
+
+g10 = g:option(Value, "akey", translate("Authentication key"))
+g10.rmempty = false
+g10.size = 40
+g10.maxlength = 32
+g10:depends({level="high"})
 
 e = m:section(TypedSection, "element", translate("Element"))
 e.addremove = true
@@ -69,7 +87,7 @@ end
 
 e4 = e:option(Value, "index", translate("Attribute Index"))
 e4.rmempty = false
-e4.datatype = "and(uinteger,min(1),max(24))"
+e4.datatype = "and(uinteger,min(1),max(32))"
 
 e5 = e:option(Flag, "delta", translate("Read by Cycle"))
 e5.default = "1"
