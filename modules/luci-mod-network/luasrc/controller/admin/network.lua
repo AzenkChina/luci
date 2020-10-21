0d8c59c24c117590f30b775e27bad97260748f49
azenk <azenk@Debian.domain>
2020-10-21 09:39:34 +0800

    Modify luci-mod-network.

--- modules/luci-mod-network/luasrc/controller/admin/network.lua
+++ modules/luci-mod-network/luasrc/controller/admin/network.lua
@@ -8,37 +8,8 @@ function index()
 	local page
 
 --	if page.inreq then
-		page = entry({"admin", "network", "switch"}, view("network/switch"), _("Switch"), 20)
-		page.uci_depends = { network = { ["@switch[0]"] = "switch" } }
-
-		page = entry({"admin", "network", "wireless"}, view("network/wireless"), _('Wireless'), 15)
+		page = entry({"admin", "system", "wireless"}, view("network/wireless"), _('Wireless'), 15)
 		page.uci_depends = { wireless = { ["@wifi-device[0]"] = "wifi-device" } }
 		page.leaf = true
-
-		page = entry({"admin", "network", "network"}, view("network/interfaces"), _("Interfaces"), 10)
-		page.leaf   = true
-		page.subindex = true
-
-		page = node("admin", "network", "dhcp")
-		page.uci_depends = { dhcp = true }
-		page.target = view("network/dhcp")
-		page.title  = _("DHCP and DNS")
-		page.order  = 30
-
-		page = node("admin", "network", "hosts")
-		page.uci_depends = { dhcp = true }
-		page.target = view("network/hosts")
-		page.title  = _("Hostnames")
-		page.order  = 40
-
-		page  = node("admin", "network", "routes")
-		page.target = view("network/routes")
-		page.title  = _("Static Routes")
-		page.order  = 50
-
-		page = node("admin", "network", "diagnostics")
-		page.target = view("network/diagnostics")
-		page.title  = _("Diagnostics")
-		page.order  = 60
 --	end
 end
